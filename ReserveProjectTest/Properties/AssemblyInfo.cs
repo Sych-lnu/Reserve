219d10a0e50ce5e4a4665b435b6820784b61c362
Sych-lnu <79633718+Sych-lnu@users.noreply.github.com>
2023-07-24 12:36:10 +0300

    Unit tests

---
+++ ReserveProjectTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReserveProjectTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReserveProjectTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a9bb03eb-d126-48aa-8098-142f5489ff76")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReserveProjectTest/UnitTest1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ReserveProject;
+
+namespace ReserveProjectTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestReserveFileNameReturn()
+        {
+            Reserve reserve = new Reserve("D:\\C# projects\\ReserveProject\\in1.txt");
+            string expectedName = "D:\\C# projects\\ReserveProject\\in1.txt";
+            FileManager.ReadInfoFromFile(reserve.FileName, reserve);
+            string actualName = reserve.FileName;
+            Assert.AreEqual(expectedName, actualName);
+        }
+        [TestMethod]
+        public void TestReserveNameReturn()
+        {
+            Reserve reserve = new Reserve("D:\\C# projects\\ReserveProject\\in1.txt");
+            FileManager.ReadInfoFromFile(reserve.FileName, reserve);
+            string expectedName = "Kyiv";
+            string actualName = reserve.Name;
+            Assert.AreEqual(expectedName, actualName);
+        }  
+        [TestMethod]
+        public void TestReserveCountOfPopulationsChangeAdd()
+        {
+            Reserve reserve = new Reserve("D:\\C# projects\\ReserveProject\\in1.txt");
+            FileManager.ReadInfoFromFile(reserve.FileName, reserve);
+            Population population = new Population();
+            int expectedCount = reserve.CountOfPopulations + 1;
+            reserve.AddPopulation(population);
+            int actualCount = reserve.CountOfPopulations;
+            Assert.AreEqual(expectedCount, actualCount);
+
+        }
+        [TestMethod]
+        public void TestReserveCountOfPopulationsChangeRemove()
+        {
+            Reserve reserve = new Reserve("D:\\C# projects\\ReserveProject\\in1.txt");
+            FileManager.ReadInfoFromFile(reserve.FileName, reserve);
+            int expectedCount = reserve.CountOfPopulations - 1;
+            reserve.RemovePopulation(1);
+            int actualCount = reserve.CountOfPopulations;
+            Assert.AreEqual(expectedCount, actualCount);
+            
+
+        }
+        [TestMethod]
+        public void TestPopulationNameByIndex()
+        {
+            Reserve reserve = new Reserve("D:\\C# projects\\ReserveProject\\in1.txt");
+            FileManager.ReadInfoFromFile(reserve.FileName, reserve);
+            string expectedPopulationName = "deer";
+            string actualPopulationName = reserve.getPopulationByIndex(0).Name;
+            Assert.AreEqual(expectedPopulationName, actualPopulationName);
+            
+        }
+        [TestMethod]
+        public void TestPopulationSizeByIndex()
+        {
+            Reserve reserve = new Reserve("D:\\C# projects\\ReserveProject\\in1.txt");
+            FileManager.ReadInfoFromFile(reserve.FileName, reserve);
+            int expectedPopulationSize = 24;
+            int actualPopulationSize = reserve.getPopulationByIndex(0).Size;
+            Assert.AreEqual(expectedPopulationSize, actualPopulationSize);
+        }
+        [TestMethod]
+        public void TestPopulationNameReturn()
+        {
+            string expectedPopulationName = "deer";
+            Population population = new Population("deer", 24);
+            string actualPopulationName = population.Name;
+            Assert.AreEqual(expectedPopulationName, actualPopulationName);
+        }     
+        [TestMethod]
+        public void TestPopulationSizeReturn()
+        {
+            int expectedPopulationName = 24;
+            Population population = new Population("deer", 24);
+            int actualPopulationName = population.Size;
+            Assert.AreEqual(expectedPopulationName, actualPopulationName);
+        }  
+        [TestMethod]
+        public void TestListMaxSizeReturn()
+        {
+            Reserve reserve = new Reserve("D:\\C# projects\\ReserveProject\\in1.txt");
+            FileManager.ReadInfoFromFile(reserve.FileName, reserve);
+            int expectedListSize = 100;
+            int actualListSize = reserve.MaxSize;
+            Assert.AreEqual(expectedListSize, actualListSize);
+        }
+        [TestMethod]
+        public void TestReadInfoFromFile()
+        {
+            Reserve reserve = new Reserve("D:\\C# projects\\ReserveProject\\test.txt");
+            FileManager.ReadInfoFromFile(reserve.FileName, reserve);
+            Reserve reserve1 = new Reserve("D:\\C# projects\\ReserveProject\\test.txt");
+            reserve1.AddPopulation(new Population("abc", 123));
+            string expectedPopulation = reserve1.getPopulationByIndex(0).Name;
+            string actualPopulation = reserve.getPopulationByIndex(0).Name;
+            Assert.AreEqual(expectedPopulation, actualPopulation);
+            
+        }
+
+
+
+
+    }
+}
