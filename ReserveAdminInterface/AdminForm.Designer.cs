89bfae21d882ae15a7ba11eedd1b462f24da0cff
Sych-lnu <79633718+Sych-lnu@users.noreply.github.com>
2023-07-24 12:33:18 +0300

    Admin Interface part

---
+++ ReserveAdminInterface/AdminForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ReserveProject
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InfoList = new System.Windows.Forms.ListBox();
+            this.InfoLabel = new System.Windows.Forms.Label();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InfoList
+            // 
+            this.InfoList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InfoList.FormattingEnabled = true;
+            this.InfoList.ItemHeight = 20;
+            this.InfoList.Location = new System.Drawing.Point(30, 94);
+            this.InfoList.Name = "InfoList";
+            this.InfoList.Size = new System.Drawing.Size(389, 264);
+            this.InfoList.TabIndex = 0;
+            this.InfoList.SelectedIndexChanged += new System.EventHandler(this.InfoList_SelectedIndexChanged);
+            // 
+            // InfoLabel
+            // 
+            this.InfoLabel.AutoSize = true;
+            this.InfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InfoLabel.Location = new System.Drawing.Point(26, 38);
+            this.InfoLabel.Name = "InfoLabel";
+            this.InfoLabel.Size = new System.Drawing.Size(0, 24);
+            this.InfoLabel.TabIndex = 1;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Enabled = false;
+            this.CancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CancelButton.Location = new System.Drawing.Point(30, 374);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 30);
+            this.CancelButton.TabIndex = 3;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Visible = false;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Enabled = false;
+            this.DeleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DeleteButton.Location = new System.Drawing.Point(149, 374);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 30);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.Text = "Ok";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Visible = false;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Enabled = false;
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddButton.Location = new System.Drawing.Point(613, 294);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 30);
+            this.AddButton.TabIndex = 5;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(536, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Population name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(536, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Population size";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(540, 143);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(540, 239);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(101, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.InfoLabel);
+            this.Controls.Add(this.InfoList);
+            this.Name = "AdminForm";
+            this.Text = "Reserve project";
+            this.Load += new System.EventHandler(this.UserForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox InfoList;
+        private System.Windows.Forms.Label InfoLabel;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
---
+++ ReserveAdminInterface/AdminForm.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ReserveProject;
+
+namespace ReserveProject
+{
+    public partial class AdminForm : Form
+    {
+        Reserve[] reservesList;
+        int listSize;
+        int page;
+        int deleteIndex;
+        int reserveIndex;
+        public AdminForm()
+        {
+            InitializeComponent();
+        }    
+        public AdminForm(Reserve[] reserves, int size)
+        {
+            InitializeComponent();
+            page = 1;
+            deleteIndex = -1;
+            reserveIndex = -1;
+            ListBox infoList = InfoList;
+            Label infoLabel = InfoLabel;
+            listSize = size;
+            reservesList = new Reserve[size];
+            for(int i = 0; i < size; i++)
+            {
+                reservesList[i] = reserves[i];
+            }
+            PrintList(reservesList, listSize, infoList, infoLabel);
+        }
+        public static void PrintList(Reserve[] reserves, int size, ListBox infoList, Label infoLabel) 
+        {
+            Sync(reserves, size);
+            infoLabel.Text = "List of Reserves";
+            infoList.Items.Clear();
+            for(int i = 0; i < size; i++)
+            {
+                infoList.Items.Add(i + 1 + ")" + reserves[i].Name);
+            }
+        }
+
+
+        private void UserForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InfoList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                CancelButton.Visible = true;
+                CancelButton.Enabled = true;
+                DeleteButton.Enabled = false;
+                DeleteButton.Visible = false;
+
+                InfoLabel.Text = "Info About Reserve:";
+                int index = InfoList.SelectedIndex;
+                reserveIndex = index;
+                InfoList.Items.Clear();
+                int count = reservesList[index].List.Length;
+                for (int i = 0; i < reservesList[index].List.Length; i++)
+                {
+                    if (reservesList[index].List[i] == null)
+                    {
+                        count = i;
+                        break;
+                    }
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    InfoList.Items.Add(i + 1 + ") Population name: " + reservesList[index].List[i].Name + ", population size: " + reservesList[index].List[i].Size);
+                    Console.WriteLine(i + 1 + ") Population name: " + reservesList[index].List[i].Name + ", population size: " + reservesList[index].List[i].Size);
+                }
+                page++;
+                AddButton.Enabled = true;
+            }
+            else
+            {
+                InfoLabel.Text = "Delete this population?";
+                deleteIndex = InfoList.SelectedIndex;
+                CancelButton.Visible = true;
+                CancelButton.Enabled = true;
+                DeleteButton.Enabled = true;
+                DeleteButton.Visible = true;
+                AddButton.Enabled = true;
+            }
+
+        }
+
+
+        public static void Sync(Reserve[] reserves, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                FileManager.WriteInfoToFile(reserves[i].FileName, reserves[i]);
+
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            deleteIndex = -1;
+            reserveIndex = -1;
+            page = 1;
+            CancelButton.Visible = false;
+            CancelButton.Enabled = false;
+            PrintList(reservesList, listSize, InfoList, InfoLabel);
+            
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (deleteIndex != -1)
+            {
+                reservesList[reserveIndex].RemovePopulation(deleteIndex);
+                Sync(reservesList, listSize);
+                deleteIndex = -1;
+                reserveIndex = -1;
+                page = 1;
+                DeleteButton.Enabled = false;
+                DeleteButton.Visible = false;
+                PrintList(reservesList, listSize, InfoList, InfoLabel);
+            }
+
+            
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if (reserveIndex!=-1 && textBox2.Text != "" && textBox3.Text != "")
+            {
+                reservesList[reserveIndex].AddPopulation(textBox2.Text, int.Parse(textBox3.Text));
+
+                textBox2.Text = "";
+                textBox3.Text = "";
+                Sync(reservesList, listSize);
+                AddButton.Enabled = false;
+                page = 1;
+                reserveIndex = -1;
+                PrintList(reservesList, listSize, InfoList, InfoLabel);
+            }
+        }
+    }
+}
